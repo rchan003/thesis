dc8ad8006b2ab9941eb7964dca1c97a3fb85e9e8
rachelchan <rachellee.chan@mail.utoronto.ca>
2026-01-18 18:08:04 -0500

    Skeleton scripts

---
+++ scripts/clean_data.py
@@ -0,0 +1,13 @@
+"""
+TODO: Docstring for scripts.clean_data
+"""
+
+import os
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/run_simulation.py
@@ -0,0 +1,13 @@
+"""
+TODO: Docstring for scripts.run_simulation
+"""
+
+import os
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
